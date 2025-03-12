4dd63a47ab53661b54b440c8660a548608e6c530
Nortover <staspushko7@gmail.com>
2025-03-12 11:31:49 +0200

    Update Trapeze.cs

--- Lab-1.3-OOP-Csharp/Trapeze.cs
+++ Lab-1.3-OOP-Csharp/Trapeze.cs
@@ -14,7 +14,7 @@ namespace TrapezeApp
         public Trapeze() => x1 = y1 = x2 = y2 = x3 = y3 = x4 = y4 = 0.0;
 
         // Конструктор з параметрами
-        public Trapeze(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4)
+         public void SetCoordinates(double x1 = 0, double y1 = 0, double x2 = 0, double y2 = 0, double x3 = 0, double y3 = 0, double x4 = 0, double y4 = 0)
         {
             this.x1 = x1;
             this.y1 = y1;
