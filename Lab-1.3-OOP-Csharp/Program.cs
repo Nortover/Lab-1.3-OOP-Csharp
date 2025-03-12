2b14f50ae87982aac59b15ddd07bc5ac92373fa4
Nortover <staspushko7@gmail.com>
2025-03-12 11:36:19 +0200

    Update Program.cs

--- Lab-1.3-OOP-Csharp/Program.cs
+++ Lab-1.3-OOP-Csharp/Program.cs
@@ -7,9 +7,9 @@ namespace TrapezeApp
         static void Main(string[] args)
         {
             // Створюємо 3 трапеції
-            Trapeze TR1 = new Trapeze(1, 2, 3, 4, 5, 6, 7, 8);
-            Trapeze TR2 = new Trapeze(8, 7, 6, 5, 4, 3, 2, 1);
-            Trapeze TR3 = new Trapeze(1, 2, 3, 4, 8, 7, 6, 5);
+            Trapeze TR1 = new Trapeze(x1: 1, y1: 2, x2: 3, y2: 4, x3: 5, y3: 6, x4: 7, y4: 8);
+            Trapeze TR2 = new Trapeze(x1: 8, y1: 7, x2: 6, y2: 5, x3: 4, y3: 3, x4: 2, y4: 1);
+            Trapeze TR3 = new Trapeze(x1: 1, y1: 2, x2: 3, y2: 4, x3: 8, y3: 7, x4: 6, y4: 5);
 
             Trapeze tSub = TR3 - 3;
             Console.WriteLine("After subtracting 3 units from TR3:" + tSub);
